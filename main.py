f3df66d86098b004d057eaf6b48234792739b95e
+futurehendrix <bochka13@list.ru>
2024-12-10 14:08:54 +0400

    update button (vk+yandex) test redirect

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import requests
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
